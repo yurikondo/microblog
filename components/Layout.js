2936e1a4878bfa90c4df8860766b52ae4a9f1ef1
yuri <yuri@example.com>
2023-05-27 18:31:25 +0900

    20. Next.jsにおけるcssスタイリング適用方法を学ぼう

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,16 @@
 import Head from "next/head";
+import styles from "./layout.module.css";
 
 const name = "yuri";
-export const subtitle = "Micro Blog ";
+export const sitetitle = "Micro Blog ";
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
-      <header>
+      <header className={styles.header}>
         <img src="/images/profile.png" />
         <h1>{name}</h1>
       </header>
