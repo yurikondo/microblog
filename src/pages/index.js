5b178c2de6e9846731cb5741e7016b1f95c8319e
yuri <yuri@example.com>
2023-05-27 19:05:32 +0900

    23. トップページのレイアウトを更新しよう

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
 import Layout from "../../components/Layout";
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <Layout></Layout>
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>エンジニア目指してます。好きな言語はJavascriptです。</p>
+      </section>
+      <section>
+        <div>
+          <article>
+            <h2>📝エンジニアのブログ</h2>
+            <Link href="/">
+              <img src="./images/thumbnail01.jpg" />
+            </Link>
+            <Link href="/">
+              <a>SSGとSSRの違いとは?</a>
+              <br />
+              <small>投稿日時</small>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </Layout>
   );
 }
