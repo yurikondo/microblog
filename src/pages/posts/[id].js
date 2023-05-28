4ff780c24bf7fd0659c688504d8cad1ba0e3542a
yuri <yuri@example.com>
2023-05-29 07:03:52 +0900

    35. getStaticPathsを実際に使ってみよう

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -1,9 +1,16 @@
 import Layout from "../../../components/Layout";
+import { getAllPostIds } from "../../../lib/post";
+
+export async function getStaticPath() {
+  const paths = getAllPostIds();
+
+  return {
+    path,
+    //[id]以外のpathを指定すると404notfoundになる
+    fallback: false,
+  };
+}
 
 export default function Post() {
-  return (
-    <Layout>
-      どう的ルーティング
-    </Layout>
-  );
+  return <Layout>どう的ルーティング</Layout>;
 }
